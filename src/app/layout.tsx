8a259c0d29daac5443773b25a1555fa7651294a5
Utkarsh <utkarshshrivastava.13@gmail.com>
2025-09-04 11:10:33 +0530

    minor-update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Sochlabs.in",
   description: "Admin + User App",
+  icons: {
+    icon: "/vercel.svg", // relative to the public/ folder
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
